201367fc76efb1a69f5687c190acee9b69e49017
zidaneNS <zidane.nur004@gmail.com>
2026-07-17 20:22:39 +0700

    remove footer

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
       <HowItWorksSection />
       <FeaturesSection />
       <SkillsSection />
-      <TokenSection />
+      {/* <TokenSection /> */}
       <Footer />
       <InstallGuide visible={guideVisible} onClose={hideGuide} />
     </>
